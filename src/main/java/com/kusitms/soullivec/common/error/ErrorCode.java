bd65c22d3cbd5cce82ecc54e255bd4de82fa12a7
emilywin825 <emilywin825@naver.com>
2024-02-28 23:08:03 +0900

    [feat] Error handler 설정

---
+++ src/main/java/com/kusitms/soullivec/common/error/ErrorCode.java
@@ -0,0 +1,45 @@
+package com.kusitms.soullivec.common.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    /**
+     *  400 Bad Request
+     */
+
+
+    /**
+     *  401 Unauthorized
+     */
+
+
+    /**
+     *  403 Forbidden
+     */
+
+
+    /**
+     *  404 Not Found
+     */
+    ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 엔티티를 찾을 수 없습니다."),
+
+    /**
+     * 409 Conflict
+     */
+    DUPLICATE_SAMPLE_TEXT(HttpStatus.CONFLICT, "이미 존재하는 TEXT입니다."),
+
+    /**
+     *  500 INTERNAL SERVER ERROR
+     */
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다."),
+    FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "S3 이미지 업로드에 실패");
+
+    private final HttpStatus status;
+    private final String message;
+}
+
